674d2470fc157ffe3df5ee7b5e5fe107a9e4a50f
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-06-11 12:41:46 -0500

    Agregando carpeta de debugging y creando primer HealthCheck endpoint.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({ path: 'HealthChecks' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
