08e219d4db757c4ce5d9592e45cabb8df4f303f7
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-06-18 14:43:07 -0500

    Seteando correctamente las propiedades del data source con variables de entorno.

--- src/infrastructure/db/migrations/MigrationsDataSource.ts
+++ src/infrastructure/db/migrations/MigrationsDataSource.ts
@@ -1,12 +1,12 @@
-import { DataSource } from 'typeorm';
 import * as path from 'path';
+import { DataSource } from 'typeorm';
 
 export const migrationDS = new DataSource({
   type: 'postgres' as const,
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'coffee1234',
-  database: 'postgres',
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT),
+  username: process.env.USER,
+  password: process.env.PSW,
+  database: process.env.DATABASE,
   migrations: [path.resolve(__dirname, 'migrations', '*')],
 });
