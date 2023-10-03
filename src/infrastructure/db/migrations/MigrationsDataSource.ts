302c2e28c6768b84d8d299d17acffbb419b3a786
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-10-03 11:54:55 -0500

    Ajuste sobre el DataSource que permite la migracion de datos. Migracion Inicial en ambiente cloud.

--- src/infrastructure/db/migrations/MigrationsDataSource.ts
+++ src/infrastructure/db/migrations/MigrationsDataSource.ts
@@ -3,10 +3,17 @@ import { DataSource } from 'typeorm';
 
 export const migrationDS = new DataSource({
   type: 'postgres' as const,
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'coffee1234',
-  database: 'postgres',
+  host: process.env.HOST,
+  port: parseInt(process.env.PG_PORT),
+  username: process.env.DB_USER,
+  password: process.env.PSW,
+  database: process.env.DATABASE,
   migrations: [path.resolve(__dirname, 'migrations', '*')],
+  ssl: process.env.SSL === 'true',
+  extra: {
+    ssl: process.env.SSL === 'true' ? {
+      rejectUnauthorized: false,
+    }
+      : null,
+  },
 });
